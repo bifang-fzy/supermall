c45030547c02ce6b4be1d7a925e9539de437a514
bifang-FZY <1316736685@qq.com>
2019-09-10 17:13:18 +0800

    init

---
+++ src/components/common/tabBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="tabBar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tabBar',
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+#tabBar{
+    width: 100%;
+    background: #fff;
+    box-shadow: 0px -2px 10px #eee;
+    position: fixed;
+    left: 0px;
+    bottom: 0px;
+    z-index: 99;
+    display: flex;
+    padding: 5px 0px;
+}
+</style>
---
+++ src/components/common/tabBarItem.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="tab_bar_item" @click="itemClick">
+        <div v-if="!isActive"><slot name="item-icon"></slot></div>
+        <div v-else><slot name="item-icon-active"></slot></div>
+        <div :style="activeStyle"><slot name="item-text"></slot></div>
+    </div> 
+</template>
+
+<script>
+export default {
+  name: 'tabBarItem',
+  props:{
+      path: String,
+      activeColor:{
+          type: String,
+          default: '#e25858'
+      }
+  },
+  computed:{
+      isActive(){
+          return this.$route.path.indexOf(this.path) !== -1
+      },
+      activeStyle(){
+          return this.isActive ? {color: this.activeColor} : {}
+      }
+  },
+  methods:{
+      itemClick(){
+          this.$router.replace(this.path)
+      }
+  }
+}
+</script>
+
+<style scoped>
+.tab_bar_item{
+    flex: 1;
+    font-size: 12px;
+    text-align: center
+}
+</style>
