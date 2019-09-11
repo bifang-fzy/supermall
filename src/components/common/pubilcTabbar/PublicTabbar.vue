0d1a216c0a6e4bfa2a831aa669960136783dbfbd
bifang-FZY <1316736685@qq.com>
2019-09-11 10:10:08 +0800

    init

--- src/components/common/pubilcTabbar/PublicTabbar.vue
+++ src/components/common/pubilcTabbar/PublicTabbar.vue
@@ -2,23 +2,23 @@
     <div id="publicTabbar">
         <tabBar>
             <tabBarItem path='/home' activeColor="#e25858">
-                <img slot="item-icon" src="@/assets/img/tabbar/home.png" alt="" />
-                <img slot="item-icon-active" src="@/assets/img/tabbar/home_active.png" alt="" />
+                <img slot="item-icon" src="~assets/img/tabbar/home.png" alt="" />
+                <img slot="item-icon-active" src="~assets/img/tabbar/home_active.png" alt="" />
                 <div slot="item-text">首页</div>
             </tabBarItem>
             <tabBarItem path='/category' activeColor="#e25858">
-                <img slot="item-icon" src="@/assets/img/tabbar/category.png" alt="" />
-                <img slot="item-icon-active" src="@/assets/img/tabbar/category_active.png" alt="" />
+                <img slot="item-icon" src="~assets/img/tabbar/category.png" alt="" />
+                <img slot="item-icon-active" src="~assets/img/tabbar/category_active.png" alt="" />
                 <div slot="item-text">分类</div>
             </tabBarItem>
             <tabBarItem path='/cart' activeColor="#e25858">
-                <img slot="item-icon" src="@/assets/img/tabbar/cart.png" alt="" />
-                <img slot="item-icon-active" src="@/assets/img/tabbar/cart_active.png" alt="" />
+                <img slot="item-icon" src="~assets/img/tabbar/cart.png" alt="" />
+                <img slot="item-icon-active" src="~assets/img/tabbar/cart_active.png" alt="" />
                 <div slot="item-text">购物车</div>
             </tabBarItem>
             <tabBarItem path='/profile' activeColor="#e25858">
-                <img slot="item-icon" src="@/assets/img/tabbar/profile.png" alt="" />
-                <img slot="item-icon-active" src="@/assets/img/tabbar/profile_active.png" alt="" />
+                <img slot="item-icon" src="~assets/img/tabbar/profile.png" alt="" />
+                <img slot="item-icon-active" src="~assets/img/tabbar/profile_active.png" alt="" />
                 <div slot="item-text">我的</div>
             </tabBarItem>
         </tabBar>
